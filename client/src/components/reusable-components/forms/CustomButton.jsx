2156f24dd795549c3ef9e44ecb759f3f7c96faf9
Ash19-88 <ayelenllampa88@gmail.com>
2024-02-13 19:43:13 -0300

    customButton

---
+++ client/src/components/reusable-components/forms/CustomButton.jsx
@@ -0,0 +1,18 @@
+// eslint-disable-next-line react/prop-types
+function CustomButton({ onClick, text, icon: Icon }) {
+  const handleClick = () => {
+    onClick();
+  };
+
+  return (
+    <button
+      className="flex items-center btn btn-xs sm:btn-sm md:btn-md lg:btn-lg"
+      onClick={handleClick}
+    >
+      {Icon && <Icon />}
+      {text}
+    </button>
+  );
+}
+
+export default CustomButton;
