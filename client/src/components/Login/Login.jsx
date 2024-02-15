e694f77dc3c8f06ddf6569bb282294bda411aba0
Ash19-88 <ayelenllampa88@gmail.com>
2024-02-15 00:37:34 -0300

    login con email y password

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -3,11 +3,46 @@ import { useEffect, useState } from "react";
 import loginbg from "../../assets/loginbg.jpg";
 import LOGO from "../../assets/LOGO.png";
 import { Link, useNavigate } from "react-router-dom";
+import CustomButton from "../reusable-components/forms/CustomButton";
+import {
+  authenticateUser,
+  sendToBackend,
+} from "../../auxFunctions/loginFunctions";
 
 function Login() {
   const navigate = useNavigate();
   const [ultimaPosicion, setUltimaPosicion] = useState({});
+  // eslint-disable-next-line no-unused-vars
   const [emailTuneMatch, setEmailTuneMatch] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleButton = () => {
+    navigate("/signup");
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (validateForm()) {
+      // solicitud de autenticación
+      const response = await authenticateUser(email, password);
+      if (response.success) {
+        // Navegamos a la página de inicio
+        navigate("/home");
+      } else {
+        // Mostramos mensaje o alerta de error
+        alert(response.message);
+      }
+    } else {
+      // Mostrar mensaje de validación fallida, se podria poner una libreria para las alertas como sweetalert o notify
+      alert("Por favor, completa los campos correctamente");
+    }
+  };
+
+  const validateForm = () => {
+    // Validamos que los campos no estén vacíos y tengan el formato correcto
+    return email.trim() !== "" && password.trim() !== "";
+  };
 
   const obtenerPosicion = () => {
     if (!navigator.geolocation) {
@@ -35,7 +70,12 @@ function Login() {
     // agregar un alert de respuesta exitosa
     console.log(response);
     // envío la info al backend
-    sendToBackend(response);
+    sendToBackend(
+      response,
+      ultimaPosicion,
+      setEmailTuneMatch,
+      handleLoginError
+    );
     navigate("/home");
   };
 
@@ -44,48 +84,8 @@ function Login() {
     console.error(error);
   };
 
-  const sendToBackend = (response) => {
-    const body =
-      Object.keys(ultimaPosicion).length !== 0
-        ? { id_token: response.credential, ultimaPosicion }
-        : { id_token: response.credential };
-
-    fetch("http://localhost:8080/api/usuario/google", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    })
-      .then((resp) => resp.json())
-      .then((res) => {
-        console.log(res);
-        localStorage.setItem(
-          "emailTuneMatch",
-          JSON.stringify(res.usuario.correo)
-        );
-        setEmailTuneMatch(res.usuario.correo);
-      })
-      .catch(handleLoginError);
-  };
-
-  const handleSignOut = async () => {
-    window.google.accounts.id.disableAutoSelect(); // google queda como variable global si se pone el script en el index principal
-    window.google.accounts.id.revoke(
-      localStorage.getItem("emailTuneMatch"),
-      (done) => {
-        if (done) {
-          console.log("Revocación exitosa");
-          localStorage.removeItem("emailTuneMatch"); // colocar el correo en el storage puede no ser seguro, pero se necesita durante la sesion para poder cerrar
-          window.location.reload(); // en caso de que haya quedado algo en el navegador se elimina, pero no es tan necesario, podría funcionar sin reload()
-        } else {
-          console.error("Error al revocar el consentimiento");
-        }
-      }
-    );
-  };
   return (
-    <div className="w-screen min-h-[130vh] sm:min-h-screen flex bg-black">
+    <div className="w-screen min-h-[140vh] sm:min-h-screen flex bg-black">
       <div className="relative">
         <div
           className="absolute top-0 left-0 w-0 h-0 border-solid border-transparent border-r-[35vw] border-b-[100vh] border-black bg-black z-10"
@@ -105,7 +105,7 @@ function Login() {
               <img src={LOGO} alt="logo" title="logo de la aplicación" />
             </li>
             <li>
-              <Link to="/">Registrarse</Link>
+              <Link to="/signup">Registrarse</Link>
             </li>
           </ul>
         </nav>
@@ -118,7 +118,7 @@ function Login() {
           }}
         >
           <div className="flex flex-col sm:flex-row justify-between w-full h-[85vh]  px-2 sm:px-4 md:px-10 z-40">
-            <div className="place-content-start flex flex-col w-11/12 mx-auto sm:mx-0 sm:w-1/2 md:w-3/4 mb-4 sm:mb-0">
+            <div className="place-content-start flex flex-col w-11/12 mx-auto justify-normal sm:justify-center sm:mx-0 sm:w-1/2 md:w-3/4 mb-4 sm:mb-0">
               <h1 className="text-2xl sm:text-3xl md:text-6xl text-start py-10">
                 Conecta con músicos!
               </h1>
@@ -128,6 +128,13 @@ function Login() {
                 ¡Inicia sesión ahora para formar parte de esta experiencia
                 musical única!
               </p>
+              <div className="flex items-center justify-center  my-5 sm:my-10">
+                <CustomButton
+                  onClick={handleButton}
+                  text={"Comienza Ahora!"}
+                  className="bg-[#BB7EBC] hover:text-[#BB7EBC] btn border-none w-1/2 md:w-1/3 text-white rounded-3xl"
+                />
+              </div>
             </div>
             <div
               className="place-content-end w-11/12 mx-auto sm:mx-0 sm:w-1/2 md:w-1/3 px-4 py-6
@@ -140,34 +147,45 @@ function Login() {
             >
               <h1 className="text-3xl text-center">Iniciar sesión</h1>
               <form
+                onSubmit={handleSubmit}
                 action="submit"
                 className="flex flex-col my-6 border-b-[3.5px] border-white pb-8"
               >
                 <div className="flex flex-col">
                   <label htmlFor="email">Email</label>
                   <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
                     style={{
                       backgroundImage: ` linear-gradient(180deg, rgba(169,181,180,1) 0%, rgba(154,201,196,1) 34%)`,
                     }}
                     className="outline-none appearance-none border-none rounded-lg px-2 py-2 text-black placeholder:text-gray-500"
-                    type="email"
                     placeholder="email@example.com"
                   />
                 </div>
                 <div className="flex flex-col">
                   <label htmlFor="password">Contraseña</label>
                   <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
                     style={{
                       backgroundImage: ` linear-gradient(180deg, rgba(169,181,180,1) 0%, rgba(154,201,196,1) 34%)`,
                     }}
                     className="outline-none appearance-none border-none rounded-lg px-2 py-2 text-black placeholder:text-gray-500"
-                    type="password"
                     placeholder="Introduce tu contraseña"
                   />
                 </div>
-                <button className="bg-[#BB7EBC] rounded-3xl my-6 p-2">
-                  Inicia Sesión
-                </button>
+                <div className="flex items-center justify-center text-center my-6">
+                  <CustomButton
+                    type="submit"
+                    className="bg-[#BB7EBC] hover:text-[#BB7EBC] btn border-none w-full text-white rounded-3xl"
+                    text={"Inicia Sesión"}
+                  />
+                </div>
               </form>
               <span className="-mt-[38px] flex items-center justify-center mx-auto text-center w-max px-1 bg-[#6C2B6D]">
                 o continua con
@@ -182,19 +200,18 @@ function Login() {
                   shape="pill"
                   // type="icon" muestra solo el icono con la G
                 />
-                <p className="my-4">
-                  ¿No tienes cuenta? <b>Registrate!</b>
-                </p>
+                <div className="my-4 flex items-center space-x-1">
+                  <p>¿No tienes cuenta?</p>
+                  <CustomButton
+                    className="hover:text-gray-400"
+                    onClick={handleButton}
+                    text={"Registrate!"}
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
-
-        {emailTuneMatch && (
-          <button id="g_id_signout" onClick={handleSignOut}>
-            Sign Out
-          </button>
-        )}
       </div>
     </div>
   );
