2e92dc2373cc3503e33ea6f40aff3bce0c8ea8ef
Tiin24 <romeroisaiasagustin@gmail.com>
2024-02-17 16:29:33 -0300

    feat[frontend]: se agrego el apartado de perfil musical para agregar al registro

--- client/src/Router/Paths.js
+++ client/src/Router/Paths.js
@@ -1,3 +1,4 @@
 export const LOGIN = "/";
 export const HOME = "/home";
 export const SIGNUP = "/signup";
+export const REGISTER2 = "/register2";
---
+++ client/src/components/reusable-components/Buttons/ButtonReg.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+
+function ButtonReg({text}) {
+  return (
+    <>
+      <button className="bg-[#BB7EBC] hover:bg-slate-800 rounded-3xl h-8">#{text}</button>
+    </>
+  );
+}
+
+export default ButtonReg;
