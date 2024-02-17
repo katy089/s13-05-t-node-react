1cee85141b3f77f4d1402e8c1eaa879d1cdd7443
Santiago Aquino <santiagoaquino01@hotmail.com>
2024-02-17 13:37:05 -0300

    fix: unique name in band and musicalGenre models

--- api/models/band.models.js
+++ api/models/band.models.js
@@ -3,13 +3,14 @@ const mongoose = require("mongoose");
 const schema = mongoose.Schema({
   name: {
     type: String,
+    unique: true,
     require: true,
   },
   status: {
     type: String,
-    enum: ['pending', 'active', 'closed'],
-    default: 'active'
-  }
+    enum: ["pending", "active", "closed"],
+    default: "active",
+  },
 });
 
 const bandModel = mongoose.model("band", schema);
--- api/models/musicalGenre.models.js
+++ api/models/musicalGenre.models.js
@@ -3,13 +3,14 @@ const mongoose = require("mongoose");
 const schema = mongoose.Schema({
   name: {
     type: String,
+    unique: true,
     require: true,
   },
   status: {
     type: String,
-    enum: ['pending', 'active', 'closed'],
-    default: 'active'
-  }
+    enum: ["pending", "active", "closed"],
+    default: "active",
+  },
 });
 
 const musicalGenreModel = mongoose.model("musicalGenre", schema);
