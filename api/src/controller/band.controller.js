7d3c27f8c4db2c700fdc1d57e9f0f510b5008884
Santiago Aquino <santiagoaquino01@hotmail.com>
2024-02-20 20:32:35 -0300

    feat: created routes allBands y allMusicalGenres

--- api/src/controller/band.controller.js
+++ api/src/controller/band.controller.js
@@ -1,14 +1,36 @@
 const bandModel = require("../models/band.models")
 
 const createBand = async (req, res) => {
-    const { name } = req.body
+    try {
+        const { name } = req.body
 
-    if (!name) return res.json({ error: "Debe ingresar el nombre de la banda" })
+        if (!name) return res.status(400).json({ error: "Debe ingresar el nombre de la banda" })
 
-    const newBand = await bandModel.create({ name })
-    res.json({ message: "Banda creada", payload: newBand })
+        const newBand = await bandModel.create({ name })
+        res.status(201).json({ message: "Banda creada", payload: newBand })
+    }
+    catch (err) {
+        res.status(500).json({
+            message: 'Hubo un error inesperado al crear los datos',
+            error: e.message,
+        })
+    }
+}
+
+const allBands = async (req, res) => {
+    try {
+        const bands = await bandModel.find({ status: 'active' })
+        res.status(200).json(bands)
+    }
+    catch (err) {
+        res.status(500).json({
+            message: 'Hubo un error inesperado al traer los datos',
+            error: e.message,
+        })
+    }
 }
 
 module.exports = {
     createBand,
+    allBands
 }
--- api/src/controller/musicalGenre.controller.js
+++ api/src/controller/musicalGenre.controller.js
@@ -1,14 +1,36 @@
 const musicalGenreModel = require("../models/musicalGenre.models")
 
 const createMusicalGenre = async (req, res) => {
-    const { name } = req.body
+    try {
+        const { name } = req.body
 
-    if (!name) return res.json({ error: "Debe ingresar el nombre del genero musical" })
+        if (!name) return res.status(400).json({ error: "Debe ingresar el nombre del genero musical" })
 
-    const newMusicalGenre = await musicalGenreModel.create({ name })
-    res.json({ message: "Genero creado", payload: newMusicalGenre })
+        const newMusicalGenre = await musicalGenreModel.create({ name })
+        res.status(201).json({ message: "Genero creado", payload: newMusicalGenre })
+    }
+    catch (err) {
+        res.status(500).json({
+            message: 'Hubo un error inesperado al crear los datos',
+            error: e.message,
+        })
+    }
+}
+
+const allMusicalGenres = async (req, res) => {
+    try {
+        const musicalGenres = await musicalGenreModel.find({ status: 'active' })
+        res.status(200).json(musicalGenres)
+    }
+    catch (err) {
+        res.status(500).json({
+            message: 'Hubo un error inesperado al traer los datos',
+            error: e.message,
+        })
+    }
 }
 
 module.exports = {
     createMusicalGenre,
+    allMusicalGenres
 }
