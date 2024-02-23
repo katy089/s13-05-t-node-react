02ce2c146761ae8f895cb5731da06d146855bc5f
AngelBlackBlue <angel.blackbkue@hotmail.com>
2024-02-23 20:53:12 -0300

    botones cambian de color con cada clic

--- client/src/components/Register22/ButtonGenero.jsx
+++ client/src/components/Register22/ButtonGenero.jsx
@@ -1,54 +1,32 @@
+import{ useState } from 'react';
+
 /* eslint-disable react/prop-types */
-const ButtonGenero = ({
-    text,
-    onClick,
+const ButtonGenero = ({ 
+    text, 
+    onClick 
+}) => {
 
-    
-    }) => {
+    const [isClicked, setIsClicked] = useState(false);
 
-    const handleRegister = () => {
-        onClick({ text });
-    }
+    const handleClick = () => {
+        setIsClicked(!isClicked);
+        onClick(text);
+    };
 
     return (
-        <>
-            <button
-                className="bg-[#c329c5] hover:bg-[nuevo-color] active:bg-[#111011] rounded-3xl h-8 focus:outline-none focus:ring focus:border-purple-300"
-                // className="bg-[#c329c5] rounded-3xl h-8"
-                style={{ width: 'auto' }}
-                onClick={handleRegister}
-             
-
-            >{text}</button>
-        </>
+        <button
+            className={`${isClicked ? 'bg-[#1f1e1f]' : 'bg-[#c329c5]'
+                } hover:bg-[nuevo-color] active:bg-[#111011] rounded-3xl h-8 focus:outline-none focus:ring focus:border-purple-300`}
+            style={{ width: 'auto' }}
+            onClick={handleClick}
+        >
+            {text}
+        </button>
     );
-}
+
+};
 
 export default ButtonGenero;
-
-// import React, { useState } from 'react';
-
-// const ButtonGenero = ({ text, onClick }) => {
-//     const [isClicked, setIsClicked] = useState(false);
-
-//     const handleClick = () => {
-//         setIsClicked(!isClicked);
-//         onClick(text);
-//     };
-
-//     return (
-//         <button
-//             className={`${isClicked ? 'bg-black' : 'bg-[#c329c5]'
-//                 } hover:bg-[nuevo-color] active:bg-[#111011] rounded-3xl h-8 focus:outline-none focus:ring focus:border-purple-300`}
-//             style={{ width: 'auto' }}
-//             onClick={handleClick}
-//         >
-//             {text}
-//         </button>
-//     );
-// };
-
-// export default ButtonGenero;
 
 
 
