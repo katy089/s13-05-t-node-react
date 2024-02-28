935a5f8a8db9d8f6a1c8deafcf7a43b07f2fb292
Santiago Aquino <santiagoaquino01@hotmail.com>
2024-02-28 16:45:20 -0300

    feat: update user services

--- api/src/services/serviceUser.js
+++ api/src/services/serviceUser.js
@@ -1,92 +1,90 @@
-const Usuario = require('../models/usuarios.models')
+const Usuario = require("../models/usuarios.models");
 
-
-
-const bycript = require('bcryptjs')
-
-const calcularDistancia = require('../../helpers/distance/haversine')
-
+const bycript = require("bcryptjs");
+const scoring = require("../../helpers/scoring");
+const calcularDistancia = require("../../helpers/distance/haversine");
 
 const coordTuneMatch = {
   lat: 60.16952,
-  lon: 24.93545
-}
+  lon: 24.93545,
+};
 
 module.exports = {
   signUp: async (nombre, correo, password, rest, res) => {
     try {
-      const salt = bycript.genSaltSync()
-      const usuario = new Usuario({ nombre, correo, password, rest })
-      usuario.password = bycript.hashSync(password, salt)
+      const salt = bycript.genSaltSync();
+      const usuario = new Usuario({ nombre, correo, password, rest });
+      usuario.password = bycript.hashSync(password, salt);
 
-      let distancia = 'No tenemos tus coordenadas'
+      let distancia = "No tenemos tus coordenadas";
 
-      if ('ultimaPosicion' in rest) {
-        const { ultimaPosicion } = rest
-        usuario.ultimaPosicion = ultimaPosicion
-        await usuario.save()
+      if ("ultimaPosicion" in rest) {
+        const { ultimaPosicion } = rest;
+        usuario.ultimaPosicion = ultimaPosicion;
+        await usuario.save();
         return res.status(201).json({
           message: `Gracias por Inscribirte ${nombre}`,
           usuario,
-          distancia
-        })
+          distancia,
+        });
       }
-      await usuario.save()
+      await usuario.save();
       return res.status(201).json({
         message: `Gracias por Inscribirte ${nombre}`,
         usuario,
-        distancia
-      })
-
-
+        distancia,
+      });
     } catch (error) {
       return res.status(400).json({
         error: `Se ha producido un error: ${error.message}`,
         type: error.name,
-        stack: error.stack
-      })
+        stack: error.stack,
+      });
     }
   },
 
   logIn: async (correo, password, rest, res) => {
-    let distancia = 'No tenemos tus coordenadas'
+    let distancia = "No tenemos tus coordenadas";
 
     try {
-      const usuario = await Usuario.findOne({ correo, activo: true })
+      const usuario = await Usuario.findOne({ correo, activo: true });
 
-      if ('ultimaPosicion' in rest) {
-        const { ultimaPosicion } = rest
-        usuario.ultimaPosicion = ultimaPosicion
-        await usuario.save()
-        distancia = calcularDistancia(ultimaPosicion, coordTuneMatch)
+      if ("ultimaPosicion" in rest) {
+        const { ultimaPosicion } = rest;
+        usuario.ultimaPosicion = ultimaPosicion;
+        await usuario.save();
+        distancia = calcularDistancia(ultimaPosicion, coordTuneMatch);
       }
-      if (!usuario) return res.status(404).json({
-        message: 'No existe este usuario',
-      })
+      if (!usuario)
+        return res.status(404).json({
+          message: "No existe este usuario",
+        });
 
-      const noCrypt = bycript.compareSync(password, usuario.password)
-      if (!noCrypt) return res.status(400).json({
-        message: 'Contraseña incorrecta',
-      })
+      const noCrypt = bycript.compareSync(password, usuario.password);
+      if (!noCrypt)
+        return res.status(400).json({
+          message: "Contraseña incorrecta",
+        });
 
       return res.status(200).json({
         message: `Gracias por volver ${usuario.nombre}`,
-        usuario, distancia
-      })
+        usuario,
+        distancia,
+      });
     } catch (error) {
       return res.status(400).json({
         error: `Se ha producido un error: ${error.message}`,
         type: error.name,
-        stack: error.stack
-      })
+        stack: error.stack,
+      });
     }
   },
 
   googleAuth: async (correo, nombre, img, ultimaPosicion, res) => {
     try {
-      const salt = bycript.genSaltSync()
-      let usuario = await Usuario.findOne({ correo })
-      let distancia = 'No tenemos tus coordenadas'
+      const salt = bycript.genSaltSync();
+      let usuario = await Usuario.findOne({ correo });
+      let distancia = "No tenemos tus coordenadas";
 
       if (!usuario) {
         const data = {
@@ -94,64 +92,136 @@ module.exports = {
           correo,
           password: bycript.hashSync(process.env.GOOGLE_PASSWORD, salt),
           fotos: [img],
-          google: true
-        }
-        usuario = new Usuario(data)
+          google: true,
+        };
+        usuario = new Usuario(data);
 
         if (ultimaPosicion) {
-          distancia = calcularDistancia(ultimaPosicion, coordTuneMatch)
-          usuario.ultimaPosicion = ultimaPosicion
+          distancia = calcularDistancia(ultimaPosicion, coordTuneMatch);
+          usuario.ultimaPosicion = ultimaPosicion;
         }
 
-        await usuario.save()
+        await usuario.save();
         return res.status(201).json({
-          message: 'Gracias por inscribirte ' + nombre,
+          message: "Gracias por inscribirte " + nombre,
           usuario,
-          distancia
-        })
+          distancia,
+        });
       }
       if (usuario && usuario.google) {
         if (ultimaPosicion) {
-          usuario.ultimaPosicion = ultimaPosicion
-          await usuario.save()
-          distancia = calcularDistancia(ultimaPosicion, coordTuneMatch)
+          usuario.ultimaPosicion = ultimaPosicion;
+          await usuario.save();
+          distancia = calcularDistancia(ultimaPosicion, coordTuneMatch);
         }
         return res.status(200).json({
           message: `Gracias por volver ${usuario.nombre}`,
           usuario,
-          distancia
-        })
+          distancia,
+        });
       }
     } catch (error) {
       return res.status(400).json({
         error: `Se ha producido un error: ${error.message}`,
         type: error.name,
-        stack: error.stack
-      })
+        stack: error.stack,
+      });
     }
   },
 
-
-
-  getUser: async (id, res) => {
+  matchProfile: async (id, res) => {
+    const start = new Date();
 
     try {
-      const user = await Usuario.findOne({ _id: id })
-      if (!user) return res.json({ error: "No existe el usuario" })
-      res.json(user)
+      const fields = ["bandas", "generos", "ultimaPosicion"];
+      const user = await Usuario.findOne({ _id: id }, fields);
+      const matchs = await Usuario.find(
+        {
+          _id: { $ne: user._id },
+          $or: [
+            { generos: { $elemMatch: { $in: user.generos } } },
+            { bandas: { $elemMatch: { $in: user.bandas } } },
+          ],
+        },
+        fields
+      ).limit(10); //.explain("executionStats");
+      // posibles problemas de performance: https://www.mongodb.com/docs/manual/reference/operator/query/in/#syntax
+      const match_list = scoring(user._doc, matchs);
 
+      const end = new Date();
+      res.status(200).json({
+        match_list,
+        estimated_time: end.getTime() - start.getTime() + "ms",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  getTuneMatch: async (id, res) => {
+    try {
+      const fields = {
+        tuneMatch: 1,
+        ultimaPosicion: 1,
+        _id: 0,
+        bandas: 0,
+        generos: 0,
+      };
+      const { tuneMatch, ultimaPosicion: userUP } = await Usuario.findOne(
+        { _id: id },
+        fields
+      );
+      const match_ids = tuneMatch.map((o) => o.id);
+      let user_profiles = null;
+
+      if (match_ids.length > 0) {
+        user_profiles = await Usuario.find(
+          {
+            _id: { $in: match_ids },
+          },
+          ["_id", "nombre", "fotos", "generos", "bandas", "ultimaPosicion"]
+        );
+
+        user_profiles.forEach((p, i) => {
+          let distance = "No es posible calcular la distancia";
+
+          if (
+            userUP.lat != null &&
+            userUP.lon != null &&
+            p["ultimaPosicion"].lat != null &&
+            p["ultimaPosicion"].lon != null
+          ) {
+            distance = calcularDistancia(p["ultimaPosicion"], userUP);
+          }
+          user_profiles[i]._doc["distancia"] = distance;
+        });
+      }
+
+      res.status(200).json({ tuneMatch: user_profiles });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  getUser: async (id, res) => {
+    try {
+      const user = await Usuario.findOne({ _id: id });
+      if (!user) return res.json({ error: "No existe el usuario" });
+      res.json(user);
     } catch (error) {
       return res.status(400).json({
         error: `Se ha producido un error: ${error.message}`,
         type: error.name,
-        stack: error.stack
-      })
-
+        stack: error.stack,
+      });
     }
-
   },
 
-  updateUser: async (id, { nombre, miGenero, distancia, bandas, generos, fotos, enBuscaDe }, res) => {
+  updateUser: async (
+    id,
+    { nombre, miGenero, distancia, bandas, generos, fotos, enBuscaDe },
+    res
+  ) => {
     try {
       const user = await Usuario.findOneAndUpdate(
         { _id: id },
@@ -161,14 +231,12 @@ module.exports = {
       if (!user) return res.json({ error: "No existe el usuario" });
 
       res.json(user);
-    }
-    catch (error) {
+    } catch (error) {
       return res.status(400).json({
         error: `Se ha producido un error: ${error.message}`,
         type: error.name,
-        stack: error.stack
-      })
-
+        stack: error.stack,
+      });
     }
-  }
-} 
+  },
+};
