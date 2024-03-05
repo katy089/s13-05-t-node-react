828f3ed9c3be954d0c8af18afb845de71119a583
Ash19-88 <ayelenllampa88@gmail.com>
2024-03-05 16:55:36 -0300

    chat url en api.js

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,7 +1,5 @@
-
 export const API_URL_BASE =
   import.meta.env.VITE_URL_BASE || "https://tunematch.onrender.com/api";
-
 
 //--------------------------------------------------------------------------
 
@@ -16,4 +14,4 @@ export const API_URL_UPDATE = API_URL_BASE + "/usuario";
 export const API_URL_REGISTER_GENEROS = API_URL_BASE + "/musicalGenre";
 export const API_URL_REGISTER_BANDAS = API_URL_BASE + "/band";
 export const API_URL_MATCHLIST = API_URL_BASE + "/usuario/list/:id";
-
+export const API_URL_CHAT = API_URL_BASE;
