0532d8314aa8169430b0e39c272ad9b53a89efb6
Ash19-88 <ayelenllampa88@gmail.com>
2024-03-05 22:35:48 -0300

    empezando a conectar tindercard al back

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -6,12 +6,13 @@ export const API_URL_BASE =
 export const API_URL_REGISTER = API_URL_BASE + "/usuario/sign-up";
 export const API_URL_LOGIN = API_URL_BASE + "/usuario/login";
 export const API_URL_GOOGLE = API_URL_BASE + "/usuario/google";
-
 export const API_URL_UPDATE = API_URL_BASE + "/usuario";
+export const API_URL_TUNEMATCHLIST =
+  API_URL_BASE + "/usuario/match/profile/:id";
 
 //---------------------------------------------------------------------------
 
 export const API_URL_REGISTER_GENEROS = API_URL_BASE + "/musicalGenre";
 export const API_URL_REGISTER_BANDAS = API_URL_BASE + "/band";
 export const API_URL_MATCHLIST = API_URL_BASE + "/usuario/list/:id";
-export const API_URL_CHAT = API_URL_BASE;
+export const API_URL_CHAT = "https://tunematch.onrender.com/";
